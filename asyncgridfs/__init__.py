0c31b57324233b02c6338e6011ea8e9bd4723733
comger <comger@gmail.com>
2014-05-05 08:12:09 +0800

    mod gridout

--- asyncgridfs/__init__.py
+++ asyncgridfs/__init__.py
@@ -26,22 +26,24 @@ def GridGF(object):
 
 def GridOut(object):
 
-    def __init__(self, root_collection, fid):
-        assert not isinstance(root_collection,(str,unicode))
+    def __init__(self, client, root_collection, fid):
+        self.client = client
+        self.root_collectin = root_collection
         self.fid = fid
         
     
     def get_file(self, callback):
-        self.__files_coll = client.connection(files_coll(root_collection))
+        """　read fid's file infomation """
+        self.__files_coll = self.client.connection(files_coll(self.root_collection))
         func = partical(initcallback,callback)
         self.__files_coll.find_one({"_id": self.fid}, callback=func)
 
     def read(self,fileobj=None callback=None):
+        """ read a file from gfs include file's infomation and data"""
         if not fileobj:
             return self.get_file(callback=self.read)
-            
 
-        self.__chunks_coll = client.connection(chunks_coll(root_collection))
+        self.__chunks_coll = self.client.connection(chunks_coll(self.root_collection))
         cond = dict(files_id = self.fid)
         def surcor_callback(res,error):
             data = StringIO()
@@ -54,4 +56,10 @@ def GridOut(object):
         self.__chunks_coll.find(cond, sort=({'n':-1}),callback=surcor_callback)
 
 
+    def list(self, callback=None):
+        """ list all filename in gfs db """
+        self.client.command('distinct',files_coll(self.root_collection), key='filename', callback=callback)
+
+    def find(self,*args, **kwargs):
+        pass
 
